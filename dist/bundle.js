d154d7eb8d9ac03e016fc8b90193226c6f00a220
ivandmitrienko <oneblackcrystal@gmail.com>
2023-09-29 17:05:21 +0300

    initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,6 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+const appElement = document.getElementById('iphone-wrapper');
+appElement.innerHTML = '<h1>Hello, iPhone!</h1>';
+/******/ })()
+;
