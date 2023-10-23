89cb4892fa963a9e816639977b2e249c81f377ee
ivandmitrienko <oneblackcrystal@gmail.com>
2023-10-23 22:54:09 +0300

    initial commit

--- src/utility/i18nex.js
+++ src/utility/i18nex.js
@@ -53,6 +53,34 @@ i18next
     document.querySelector("h1").innerHTML = i18next.t(
       "Get Unlimited <br>Access"
     );
-    // console.log(t("nested.key"));
-    // console.log(t("great", { name: "Marcin" }));
+    document.querySelector(".creation").innerHTML = i18next.t(
+      "Unlimited Art <br>Creation"
+    );
+    document.querySelector(".styles").innerHTML = i18next.t(
+      "Exclusive <br>Styles"
+    );
+    document.querySelector(".avatars").innerHTML = i18next.t(
+      "Magic Avatars <br>With 20% Off"
+    );
+    document.querySelector(".avatars").innerHTML = i18next.t(
+      "Magic Avatars <br>With 20% Off"
+    );
+    document.querySelector(".yearly-access").innerHTML =
+      i18next.t("YEARLY ACCESS");
+    document.querySelector(".per-year").innerHTML = i18next.t(
+      "Just {{price}} per year",
+      { price: "$39.99" }
+    );
+    document.querySelector(".per-week-economy").innerHTML = i18next.t(
+      "{{price}} <br>per week",
+      { price: "$0.48" }
+    );
+    document.querySelector(".weekly-access").innerHTML =
+      i18next.t("WEEKLY ACCESS");
+    document.querySelector(".per-week").innerHTML = i18next.t(
+      "{{price}} <br>per week",
+      { price: "$6.99" }
+    );
+    document.querySelector(".button-continue").innerHTML =
+      i18next.t("Continue");
   });
