882f4bb95dc75ef5f3a7a69743401b1499f2c72b
ivandmitrienko <oneblackcrystal@gmail.com>
2023-10-26 22:36:37 +0300

    initial commit

--- src/utility/buttonContinue.js
+++ src/utility/buttonContinue.js
@@ -1,6 +1,10 @@
-const button_continue = document.querySelector('.button-continue');
+const button_continue = document.querySelector(".button-continue");
 
-button_continue.addEventListener('click',()=>{
-    let href = document.querySelector(".active-button").dataset.email;
-    window.location.href=href;
-})
+button_continue.addEventListener("click", () => {
+  let href = document.querySelector(".active-button");
+  if (href) {
+    window.location.href = href.dataset.email;;
+  } else {
+    return;
+  }
+});
